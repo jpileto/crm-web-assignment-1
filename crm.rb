1eb7a7279533b674c9e135381e9f330ad5893220
jpileto <jp.ileto@icloud.com>
2017-04-04 14:49:24 -0400

    add the post & redirect method for the add new contact feature

--- crm.rb
+++ crm.rb
@@ -4,9 +4,9 @@
 require_relative 'contact'
 require 'sinatra'
 
-Contact.create("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-Contact.create("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-Contact.create("Steve", "Jobs", "steve@apple.com", "Visionary")
+# Contact.create("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+# Contact.create("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+# Contact.create("Steve", "Jobs", "steve@apple.com", "Visionary")
 
 get '/' do
   @crm_app_name = "JP's CRM"
@@ -20,4 +20,9 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to ('/contacts')
 end
